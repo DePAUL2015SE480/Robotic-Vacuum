e87dbd8b6cb8a74c3de3deda23eedf7129b45470
Deonte D Johnson <Jet2kus84@gmail.com>
2015-10-07 13:50:43 -0500

    initializing team project

---
+++ src/depaul/edu/robotic/vacuum/RoboticVacuumMain.java
@@ -0,0 +1,15 @@
+package depaul.edu.robotic.vacuum;
+
+/**
+ * 
+ * @author Deonte Johnson
+ * 
+ * Main class where robotic vacuum
+ * application will be executed.
+ *
+ */
+public class RoboticVacuumMain {
+	public static void main(String[] args) {
+		
+	}
+}
