85f8343936f5fd36a7495fd61dae6e9540f75d79
Deonte D Johnson <Jet2kus84@gmail.com>
2015-10-08 17:45:01 -0500

    created package location for data logging

---
+++ src/depaul/edu/robotic/vacuum/diagnostics/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Deonte Johnson
+ * 
+ * This class will be used for logging data from 
+ * robot cleaning activity
+ *
+ */
+package depaul.edu.robotic.vacuum.diagnostics;
